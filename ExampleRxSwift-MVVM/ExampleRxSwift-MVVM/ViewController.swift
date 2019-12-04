364283cd380f2ab264c77fc52c05f11f54daca96
DongNV <dongnv@tinhvan.com>
2019-12-04 10:19:07 +0700

    init

---
+++ ExampleRxSwift-MVVM/ExampleRxSwift-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleRxSwift-MVVM
+//
+//  Created by MT382 on 12/4/19.
+//  Copyright © 2019 DongNV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
